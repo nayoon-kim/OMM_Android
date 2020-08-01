ac27927e87359226867f86b6144d878223120eeb
JEEWOO SUL <39071676+jeewoo1025@users.noreply.github.com>
2020-08-01 11:20:26 +0900

    TMap Marker & Lines ver 1

--- app/src/main/java/com/example/ohmymoney/PlanActivity2.java
+++ app/src/main/java/com/example/ohmymoney/PlanActivity2.java
@@ -2,8 +2,14 @@ package com.example.ohmymoney;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,9 +27,18 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import com.skt.Tmap.TMapData;
+import com.skt.Tmap.TMapMarkerItem;
+import com.skt.Tmap.TMapPOIItem;
+import com.skt.Tmap.TMapPoint;
+import com.skt.Tmap.TMapPolyLine;
 import com.skt.Tmap.TMapView;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
 public class PlanActivity2 extends AppCompatActivity {
     private LinearLayout layoutTmap;
@@ -31,6 +46,8 @@ public class PlanActivity2 extends AppCompatActivity {
     private ListView listView;
 
     private ListViewAdapter adapter;
+    private List<Address> places;
+    private String[] strList = { "감천문화마을", "BIFF광장", "이바구길" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,11 +61,71 @@ public class PlanActivity2 extends AppCompatActivity {
         // Tmap 설정
         tMapView = new TMapView(this);
         setTMapView();
+        setMarksOnMap();
 
         // 리스트뷰&어탭터
         adapter = new ListViewAdapter();
         listView.setAdapter(adapter);
         addListViewItems();
+    }
+
+    private void setMarksOnMap()
+    {
+        Thread thread = new Thread();
+
+        // 지도 위에 장소 추가
+        places = new ArrayList<>();
+        final Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        String errMesg = "";
+        TMapData tMapData = new TMapData();
+
+        for(String item : strList) {
+            try {
+               List<Address> list = geocoder.getFromLocationName(item, 1);
+
+                if(list != null && list.size() != 0) {
+                    places.add(list.get(0));
+                }
+            } catch (IOException e) {
+                errMesg = e.getMessage();
+                Log.e("map", "입출력 오류 - 서버에서 주소 변환시 에러발생");
+            } catch (IllegalArgumentException illegalArgumentException) {
+                // 잘못된 GPS 가져왔을때
+                errMesg = illegalArgumentException.getMessage();
+                Log.e("map", "잘못된 GPS 가져옴");
+            } catch (Exception ex) {
+                errMesg = ex.getMessage();
+            }
+
+            if(!errMesg.isEmpty() || !errMesg.equals("")) {
+                return;
+            }
+        }
+
+        // 마커 생성
+        ArrayList<TMapPoint> listTMapPoint = new ArrayList<>();
+        for(int i = 0; i < places.size(); i++) {
+            TMapMarkerItem markerItem = new TMapMarkerItem();
+            listTMapPoint.add(new TMapPoint(places.get(i).getLatitude(), places.get(i).getLongitude()));         // 위도, 경도
+
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.marker);
+            markerItem.setIcon(bitmap);                 // 마커 아이콘 지정
+            markerItem.setPosition(0.5f, 1.0f);
+            markerItem.setTMapPoint(listTMapPoint.get(i));         // 좌표 지정
+            markerItem.setName(strList[i]);             // 마커의 타이틀 지정
+            tMapView.addMarkerItem("marker" + Integer.toString(i), markerItem);
+        }
+        tMapView.setCenterPoint(listTMapPoint.get(0).getLongitude() + 0.001f, listTMapPoint.get(0).getLatitude() - 0.005f); // 경도, 위도
+
+        // 선 그리기
+        TMapPolyLine tMapPolyLIne = new TMapPolyLine();
+        tMapPolyLIne.setLineColor(Color.YELLOW);
+        tMapPolyLIne.setOutLineColor(Color.YELLOW);
+        tMapPolyLIne.setLineWidth(2);
+        for(int i = 0; i < listTMapPoint.size(); i++) {
+            tMapPolyLIne.addLinePoint(listTMapPoint.get(i));
+        }
+        tMapView.addTMapPolyLine("Line1", tMapPolyLIne);
     }
 
     private void getViewObject() {
@@ -67,11 +144,11 @@ public class PlanActivity2 extends AppCompatActivity {
     }
 
     public void setTMapView() {
-        // 맵 초기화
+        // tmap 맵 초기화
         tMapView.setSKTMapApiKey("l7xx4d7fa19c1d544d25beb0e537d32cca36");
         tMapView.setCompassMode(false);
         tMapView.setIconVisibility(true);
-        tMapView.setZoomLevel(15);
+        tMapView.setZoomLevel(15);              // zoom level : 1(멀리)~19(확대)
         tMapView.setMapType(TMapView.MAPTYPE_STANDARD);
         tMapView.setLanguage(TMapView.LANGUAGE_KOREAN);
         tMapView.setTrackingMode(false);
@@ -138,10 +215,9 @@ public class PlanActivity2 extends AppCompatActivity {
     }
 
     public void addListViewItems() {
-        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.icon_1), "광화문", "대한민국 서울특별시 종로구 사직로 161");
-        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.icon_2), "홍대입구역", "서울특별시 서교동");
-        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.icon_3), "롯데월드", "서울특별시 송파구 잠실동 올림픽로 240");
-        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.icon_4), "롯데월드타워",  "서울특별시 송파구 잠실6동 올림픽로 300");
+        for(int i = 0; i < places.size(); i++) {
+            adapter.addItem(ContextCompat.getDrawable(this, R.drawable.icon_1), strList[i], places.get(i).getAddressLine(0));
+        }
     }
 }
 
@@ -171,57 +247,3 @@ class ListViewItem {
     }
 }
 
-class ListViewAdapter extends BaseAdapter {
-    private ArrayList<ListViewItem> itemList = new ArrayList<>();
-
-    public ListViewAdapter() {
-    }
-
-    @Override
-    public int getCount() {
-        return itemList.size();
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        final int pos = position;
-        final Context context = parent.getContext();
-
-        if(convertView == null) {
-            LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.listview_item, parent, false);
-        }
-
-        ImageView imgNum = (ImageView)convertView.findViewById(R.id.imgNum);
-        TextView txtTitle = (TextView)convertView.findViewById(R.id.txtTitle);
-        TextView txtDesc = (TextView)convertView.findViewById(R.id.txtDesc);
-
-        ListViewItem listViewItem = itemList.get(position);
-
-        imgNum.setImageDrawable(listViewItem.getIcon());
-        txtTitle.setText(listViewItem.getTitle());
-        txtDesc.setText(listViewItem.getDesc());
-
-        return convertView;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return itemList.get(position);
-    }
-
-    public void addItem(Drawable icon, String title, String desc) {
-        ListViewItem item = new ListViewItem();
-
-        item.setIcon(icon);
-        item.setTitle(title);
-        item.setDesc(desc);
-
-        itemList.add(item);
-    }
-}
